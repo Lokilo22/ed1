97b7617b2cfb6b95d0991431bda8d978a75648dc
Lokilo22 <vitor.c.j@hotmail.com>
2021-08-29 11:44:03 -0300

    main

--- vetor_dinamico_exemplo/src/vetor_dinamico_ex.cpp
+++ vetor_dinamico_exemplo/src/vetor_dinamico_ex.cpp
@@ -9,7 +9,41 @@ int main(void){
     printf("Informe o numero de componentes do vetor\n");
     scanf("%d", &num_componentes);
 
-    v = (float *) malloc(num_componentes * sizeof(float));
+    /*v = (float *) malloc(num_componentes * sizeof(float));*/
+    v = (float *) calloc(num_componentes, sizeof(float));
 
+    for (i = 0; i < num_componentes; i++){
+        printf("%.2f\n",v[i]);
+    }
+
+    for (i = 0; i < num_componentes; i++){
+        printf("\nDigite o valor para a posicao %d do vetor: ", i+1);
+        scanf("%f",&v[i]);
+    }
+
+    printf("\n******** Valores do vetor dinamico ***********\n\n");
+
+    for ( i = 0; i < num_componentes; i++){
+        printf("%.2f\n",v[i]);
+    }
+
+    int novo_tam;
+    printf("\nInforme novo tamanho para redimensionamento: ");
+    scanf("%d", &novo_tam);
     
+    if(novo_tam > 0 /*&& novo_tam < num_componentes*/){
+        v = (float *) realloc(v, novo_tam);
+        printf("\nValor contidos: \n");
+        for (i = 0; i < novo_tam; i++){
+            printf("%.2f\n",v[i]);
+        }
+        
+    }
+    else
+        printf("\nNovo tamanho eh invalido!\n");
+
+    free(v);
+
+    getch();
+    return 0;
 }
